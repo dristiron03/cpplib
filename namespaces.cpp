11dc365a19c6ddd2ea7fe5b76ca29d9885a1c4d0
dristiron03 <dristirons@gmail.com>
2021-10-06 14:55:52 +0530

    Templates and Namespaces

---
+++ namespaces.cpp
@@ -0,0 +1,39 @@
+/*
+The functionality of namespaces is especially useful in the case that there is a
+possibility that a global object or function uses the same identifier as another one,
+causing redefinition errors.
+*/
+
+// namespaces
+#include <iostream>
+using namespace std;
+
+namespace first {
+int var = 5;
+}
+
+namespace second {
+double var = 3.1416;
+}
+
+int main () {
+	cout << first::var << endl;
+	cout << second::var << endl;
+
+
+	// using namespace second;
+	// cout << var << "\n";
+
+
+	// We could do something likes this, group into blocks
+
+	{
+		using namespace first;
+		cout << var << endl;
+	}
+	{
+		using namespace second;
+		cout << var << endl;
+	}
+	return 0;
+}
