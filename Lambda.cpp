425d2f34361ea771e123142de3e9c56ea8477670
dristiron03 <dristirons@gmail.com>
2021-10-18 18:29:42 +0530

    Update

---
+++ Lambda.cpp
@@ -0,0 +1,104 @@
+// Resource: https://docs.microsoft.com/en-us/cpp/cpp/examples-of-lambda-expressions?view=msvc-160
+
+#include<iostream>
+#include<functional>
+#include<algorithm>
+using namespace std;
+
+void Example1() {
+
+	cout << "Example1: -------------------------------\n";
+	auto f1 = [](int x, int y) {return x + y;};
+	cout << f1(3, 4) << "\n"; // output: 7
+
+	cout << [](int x, int y) {return x + y;}(3, 4) << "\n"; // output: 7
+
+	function<int(int, int)>f2 = [](int x, int y) {return x + y;};
+	cout << f2(3 , 4) << "\n"; // output: 7
+}
+
+void Example2() {
+	// Sorting by absolute value
+	cout << "Example2: -------------------------------\n";
+
+	int ar[] = {1, 5, 65, 23, -3, -7, -43, -2, -54, 45, 2};
+
+	int n = sizeof(ar) / sizeof(int);
+	sort(ar, ar + n, [&](int x, int y) {
+		return abs(x) < abs(y);
+	});
+
+	for (int &x : ar) {
+		cout << x << " ";
+	}
+	cout << "\n";
+}
+
+void Example3() {
+	// compiler binds a lambda expression to its captured
+	// variables when the expression is declared instead
+	// of when the expression is called.
+
+	cout << "Example3: -------------------------------\n";
+
+	int i = 3;
+	int j = 5;
+
+	// The following lambda expression captures i by value and
+	// j by reference.
+	function<int (void)> f = [i, &j] { return i + j; };
+
+	// Change the values of i and j.
+	i = 22;
+	j = 44;
+
+	// Call f and print its result.
+	cout << f() << endl;
+}
+
+void Example4() {
+
+	cout << "Example4: -------------------------------\n";
+
+	// How to write recursive function in lambda
+
+	int n = 12345;
+
+	// Function itself as a parameter
+	auto printReverse = [&](auto && printReverse) {
+		if (n == 0)
+			return;
+		cout << n % 10 << " ";
+		n = n / 10;
+		printReverse(printReverse);
+	};
+
+	// Function as an argument
+	printReverse(printReverse);
+
+	cout << "\n";
+	n = 12345;
+
+	function<void()> printRev = [&]() {
+		if (n == 0)
+			return;
+
+		// Defination
+		cout << n % 10 << " ";
+		n /= 10;
+		printRev();
+	};
+
+	printRev();
+	cout << "\n";
+}
+
+int main() {
+	Example1();
+
+	Example2();
+
+	Example3();
+
+	Example4();
+}
---
+++ VirtualFunction.cpp
@@ -0,0 +1,2 @@
+// Resource: https://docs.microsoft.com/en-us/cpp/cpp/virtual-functions?view=msvc-160
+
